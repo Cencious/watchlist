e4f1bc2f60fc102933c755185448742242346d04
Cencious <user.email>
2022-04-29 21:10:44 +0300

    add .

--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 from flask import render_template,request,redirect,url_for
 from app import app
 from .request import get_movies,get_movie,search_movie
-from .models import reviews
+from .models import review
 from .forms import ReviewForm
-Review = reviews.Review
+Review = review.Review
 
 
 @app.route('/')
@@ -33,8 +33,9 @@ def movie(id):
     '''
     movie= get_movie(id)
     title = f'{movie.title}'
+    reviews = Review.get_reviews(movie.id)
 
-    return render_template('movie.html',title=title,movie=movie)
+    return render_template('movie.html',title = title,movie = movie,reviews = reviews)
 
 @app.route('/search/<movie_name>')
 def search(movie_name):
@@ -45,7 +46,19 @@ def search(movie_name):
     movie_name_format="+".join(movie_name_list)
     searched_movies= search_movie(movie_name_format)
     title= f'search results for {movie_name}'
-    return render_template('search.html',movie=searched_movies)
+    return render_template('search.html',movies=searched_movies)
 
+@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+def new_review(id):
+    form = ReviewForm()
+    movie = get_movie(id)
 
-    
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+        new_review = Review(movie.id,title,movie.poster,review)
+        new_review.save_review()
+        return redirect(url_for('movie',id = movie.id ))
+
+    title = f'{movie.title} review'
+    return render_template('new_review.html',title = title, review_form=form, movie=movie)
