7163d85b45e4961101fa7c5dacd127d7b14349aa
Cencious <user.email>
2022-04-30 11:56:26 +0300

    create review and movie models

--- app/models.py
+++ app/models.py
@@ -18,12 +18,17 @@ class Review
         self.title = title
         self.imageurl = imageurl
         self.review = review
+
     def save_review(self):
         Review.all_reviews.append(self)
 
     @classmethod
-    def get_reviews(cls,id):
+    def clear_reviews(cls):
+        Review.all_reviews.clear()
 
+    @classmethod
+    def get_reviews(cls,id):
+        
         response = []
 
         for review in cls.all_reviews:
