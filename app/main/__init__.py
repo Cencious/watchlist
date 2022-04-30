2d8421245f1618cc0709cd006a9207f61e9f92c4
Cencious <user.email>
2022-04-30 11:39:54 +0300

    import blueprint class

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,3 +1,12 @@
 from flask import Blueprint
+    '''
+    Application Blueprint for defining routes.
+    '''
+
 main = Blueprint('main',__name__)
-from . import views,errors
+#The Blueprint class takes in 2 arguments. The name of the blueprint and the __name__ variable to find the location of the blueprint.
+
+
+from . import views,errors #To avoid circular dependencies we import the views and errors modules.
+
+
