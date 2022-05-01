52702980a0e7f43fd218b3f18b904e694adff116
Cencious <user.email>
2022-05-01 21:32:56 +0300

    add defination comment

--- app/request.py
+++ app/request.py
@@ -8,6 +8,7 @@ api_key =None
 base_url = None
 
 def configure_request(app):
+    #a function that takes in the application instance and replaces the values of the None variables to application configuration objects.
     global api_key,base_url
     api_key = app.config['MOVIE_API_KEY']
     base_url = app.config['MOVIE_API_BASE_URL']
