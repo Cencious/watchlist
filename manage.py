a45a701dc328fef72419b47fd8b5b833292bb41c
Cencious <user.email>
2022-05-07 05:56:55 +0300

    add shell manager decorator to create a shell context

--- manage.py
+++ manage.py
@@ -1,11 +1,14 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager, Server
+from app.models import User
+
 
 # Creating app instance
 app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server', Server)
+from app.models import User,Role
 
 @manager.command
 def test():
@@ -13,6 +16,12 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    #the make_shell_context function allows us to pass in some properties into our shell. 
+    return dict(app = app,db = db,User = User, Role = Role )
+    
 
 if __name__ == '__main__':
     manager.run()
