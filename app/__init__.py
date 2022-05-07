0f52352450bea49e455a302eaa707d1a04008405
Cencious <user.email>
2022-05-07 05:54:13 +0300

    import SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,11 @@ from ensurepip import bootstrap
 from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
 #bootstrap
 bootstrap = Bootstrap()
+db =SQLAlchemy()
 
 def create_app(config_name): # function that takes the configuration setting key as an argument
     app = Flask(__name__)
@@ -14,6 +16,8 @@ def create_app(config_name): # function that takes the configuration setting key
 
     # Initializing flask extension
     bootstrap.init_app(app)
+    db.init_app(app)
+
 
     # Registering blueprint
     from .main import main as main_blueprint
