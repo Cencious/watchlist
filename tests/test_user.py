4e0e79fb934c14ab2cef3a5b4997a61654bcc4ee
Cencious <user.email>
2022-05-07 11:21:40 +0300

    create a second test case to confirm app raises attribute error

--- tests/test_user.py
+++ tests/test_user.py
@@ -9,4 +9,17 @@ class UserModelTest(unittest.TestCase):
     def test_password_setter(self): #this ascertains that when password is being hashed and the pass_secure contains a value.
         self.assertTrue(self.new_user.pass_secure is not None)
 
+    class UserModelTest(unittest.TestCase):
+
+    def setUp(self):#creates an instance of our User class
+        self.new_user = User(password = 'banana')
+
+    def test_password_setter(self): #this ascertains that when password is being hashed and the pass_secure contains a value.
+        self.assertTrue(self.new_user.pass_secure is not None)
+
+    def test_no_access_password(self):
+            with self.assertRaises(AttributeError):
+                self.new_user.password
     
+    def test_password_verification(self):
+            self.assertTrue(self.new_user.verify_password('banana'))
