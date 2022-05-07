72d5d7c6017734597b05e576535c11aa909cf9d5
Cencious <user.email>
2022-05-07 11:33:53 +0300

    import login manager from flask-login extension

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,12 @@ from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 #bootstrap
 bootstrap = Bootstrap()
