e278b25b31315ede5a802054c2c0982bff20ca3b
Cencious <user.email>
2022-05-07 11:20:22 +0300

    create setup method that creates an instance in our user class

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,3 +1,12 @@
 import unittest
 from app.models import User
 
+class UserModelTest(unittest.TestCase):
+
+    def setUp(self):#creates an instance of our User class
+        self.new_user = User(password = 'banana')
+
+    def test_password_setter(self): #this ascertains that when password is being hashed and the pass_secure contains a value.
+        self.assertTrue(self.new_user.pass_secure is not None)
+
+    
